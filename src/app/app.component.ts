95631c3563fbe5683268cc4f26c284f2f70ad8dc
farhana.d <farhana.d@blr.cordis>
2019-07-25 11:10:07 +0530

    reference in array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sample';
+b2: any = [];
+a1: any = {descripton : [
+  {variableName: 'student.Name'},
+ {variableName: 'student.id', },
+{variableName: 'student.bag',},
+ {variableName: 'student.books', },
+ {variableName: 'student.vehicle'},
+]};
+ngOnInit() {
+  console.log('hiii');
+  this.setMethod();
+
+}
+setMethod() {
+  console.log(this.a1);
+  // this.a1.disp = []
+  this.a1.disp = Object.assign([{}], this.a1.descripton);
+  for (let i = 0 ; i < this.a1.descripton.length; i++) {
+    console.log(this.a1.descripton[i]);
+  //  this.a1.disp[i].variableName = this.a1.disp[i].variableName.split('.');
+   this.a1.disp[i] = this.a1.disp[i].variableName.split('.');
+
+
+ }
+  console.log('*****');
+  console.log(this.a1);
+ }
 }
